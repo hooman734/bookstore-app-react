b78ebf8161bd57ccd022ab5b13ee17f7869f9b6f
hooman734 <hesamyan@outlook.com>
2023-02-12 22:53:27 +0400

    added button to submit

--- src/components/BookCreate/index.tsx
+++ src/components/BookCreate/index.tsx
@@ -1,16 +1,30 @@
 import React from "react";
+import './index.css'
 
 interface BookCreateProps {
     onCreate: (name: string) => void;
 }
 function BookCreate({ onCreate }: BookCreateProps) {
 
+    const { useState } = React
+    const [title, setTitle] = useState('')
     function onInputHandle(e: React.FormEvent<HTMLInputElement>) {
-        onCreate(e.currentTarget.value)
+        setTitle(e.currentTarget.value)
+    }
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        onCreate(title)
     }
 
     return (
-        <input onInput={onInputHandle}/>
+      <div className="BookCreate">
+          <form className="input-form" onSubmit={handleSubmit}>
+              <label>Input Book's Title</label>
+              <input onInput={onInputHandle}/>
+              <button>OK</button>
+          </form>
+      </div>
     )
 }
 
